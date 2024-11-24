416a758d7f64d6292663a2dfb27fa6a758f44d16
Papariemann <hasirejan@outlook.com>
2024-11-24 16:31:06 +0100

    Made program cleaner and better and shit, everything is basically a bit different

--- src/main.rs
+++ src/main.rs
@@ -11,77 +11,40 @@ fn main() {
 
     loop {
         println!("Enter input: (see --help for commands)");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let mode_input = input.trim();
+        let mode_input = read_input().trim().to_string();
 
-        if mode_input == "n" {
-            let task = create_task();
-            let task_clone = task.task.clone();
-            task_list.push(task); // adding it to the array/vector of tasks
-            println!("Task {}: {}", task_list.len(), task_clone);
-        } else if mode_input == "d" {
-            for (i, task) in task_list.iter().enumerate() {
-                println!("{}. {} [{}]", i + 1, task.task, task.status);
+        match mode_input.as_str() {
+            "n" => {
+                let task = create_task();
+                let task_clone = task.task.clone();
+                task_list.push(task);
+                println!("Task {}: {}", task_list.len(), task_clone);
             }
-
-            println!("Enter ToDo to delete: (Enter ToDo contents or ToDo index)");
-            let mut todo_to_del = String::new();
-            io::stdin().read_line(&mut todo_to_del).unwrap();
-            let del_todo = todo_to_del.trim();
-
-            if let Ok(index) = del_todo.parse::<usize>() {
-                if index > 0 && index <= task_list.len() {
-                    // Adjust cuz list begins at task 1 but task_list begins at 0
-                    let pos = index - 1;
-                    task_list.remove(pos);
-                } else {
-                    println!("Invalid index. ");
-                }
-            } else {
-                if let Some(pos) = task_list.iter().position(|x| x.task == del_todo) {
-                    task_list.remove(pos);
-                } else {
-                    println!("Task not found. ");
-                }
+            "d" => {
+                ls(&task_list);
+                let todo_to_del = read_input().trim().to_string();
+                delete_task(&mut task_list, &todo_to_del);
             }
-        } else if mode_input == "ls" {
-            ls(&task_list);
-        } else if mode_input == "f" {
-            // if input in "normal" mode is f,
-            // a task like "go shower: in progress" -> "finished"
-            println!("Choose task to mark as completed: (Enter ToDo contents or ToDo index)");
-            ls(&task_list);
-            let mut change_status_input = String::new();
-            io::stdin().read_line(&mut change_status_input).unwrap();
-
-            if let Ok(index) = change_status_input.trim().parse::<usize>() {
-                if index > 0 && index <= task_list.len() {
-                    let pos = index - 1;
-                    task_list[pos].status = String::from("Finished");
-                    ls(&task_list);
-                } else {
-                    println!("Invalid index. ");
-                }
-            } else {
-                if let Some(pos) = task_list
-                    .iter_mut()
-                    .position(|x| x.task == change_status_input.trim())
-                {
-                    task_list[pos].status = String::from("Finished");
-                    ls(&task_list);
-                }
+            "ls" => ls(&task_list),
+            "f" => {
+                ls(&task_list);
+                let change_status_input = read_input().trim().to_string();
+                finish_task(&mut task_list, &change_status_input);
             }
-        } else if mode_input == "--help" {
-            println!("Welome to Todolist version 1");
-            println!("Available commands:");
-            println!("\"n\": Create new ToDo");
-            println!("\"d\": Delete ToDo");
-            println!("\"f\": Cross out ToDo\n");
-        } else {
-            println!("Command not recognised. Enter --help for more info on usable commands.");
+            "--help" => {
+                show_help();
+            }
+            _ => println!("Command not recognised. Enter --help for more info."),
         }
     }
+}
+
+fn read_input() -> String {
+    let mut input = String::new();
+    if io::stdin().read_line(&mut input).is_err() {
+        println!("Error reading input.");
+    }
+    input
 }
 
 fn create_task() -> Task {
@@ -102,6 +65,42 @@ fn ls(task_list: &Vec<Task>) {
             println!("{}. {} [{}]", i + 1, task.task, task.status);
         }
     }
+}
+
+fn delete_task(task_list: &mut Vec<Task>, todo_to_del: &str) {
+    if let Ok(index) = todo_to_del.parse::<usize>() {
+        if index > 0 && index <= task_list.len() {
+            task_list.remove(index - 1);
+        } else {
+            println!("Invalid index.");
+        }
+    } else if let Some(pos) = task_list.iter().position(|x| x.task == todo_to_del) {
+        task_list.remove(pos);
+    } else {
+        println!("Task not found.");
+    }
+}
+
+fn finish_task(task_list: &mut Vec<Task>, todo_to_mark: &str) {
+    if let Ok(index) = todo_to_mark.parse::<usize>() {
+        if index > 0 && index <= task_list.len() {
+            task_list[index - 1].status = String::from("Finished");
+        } else {
+            println!("Invalid index.");
+        }
+    } else if let Some(pos) = task_list.iter_mut().position(|x| x.task == todo_to_mark) {
+        task_list[pos].status = String::from("Finished");
+    } else {
+        println!("Task not found.");
+    }
+}
+
+fn show_help() {
+    println!("Welome to Todolist version 1");
+    println!("Available commands:");
+    println!("\"n\": Create new ToDo");
+    println!("\"d\": Delete ToDo");
+    println!("\"f\": Cross out ToDo\n");
 }
 
 struct Task {
